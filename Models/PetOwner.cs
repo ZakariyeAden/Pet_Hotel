285083cb66a84779b464a91bbdce2ec65325ce56
Aubrey Graves <grav0165@gmail.com>
2023-08-17 09:28:03 -0500

    Added ID and name class to PetOwner

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,11 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
 }
